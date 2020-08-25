211843e9379a21ec3ba2ea8f68bf47f55972c67c
Andrew Bateman <gomezbateman@yahoo.com>
2020-08-25 17:52:48 +0200

    file structure simplified, readme replaced, code commented

--- README.md
+++ README.md
@@ -1,3 +1,74 @@
-# FCC-Visualisation-D3-Scatterplot
+# :zap: FCC Visualisation D3 Scatterplot
 
-initial commit. 
+* D3.js used to visualise json data in a HTML scatterplot graph
+* This was a [FreeCodeCamp Data Visualisation Project](https://www.freecodecamp.org/learn/data-visualization/data-visualization-projects/visualize-data-with-a-scatterplot-graph) for Front End Certification that is still in the FCC curriculum with the same json data file
+
+*** Note: to open web links in a new window use: _ctrl+click on link_**
+
+## :page_facing_up: Table of contents
+
+* [:zap: FCC Visualisation D3 Scatterplot](#zap-fcc-visualisation-d3-scatterplot)
+  * [:page_facing_up: Table of contents](#page_facing_up-table-of-contents)
+  * [:books: General info](#books-general-info)
+  * [:camera: Screenshots](#camera-screenshots)
+  * [:signal_strength: Technologies](#signal_strength-technologies)
+  * [:floppy_disk: Setup](#floppy_disk-setup)
+  * [:computer: Code Examples](#computer-code-examples)
+  * [:cool: Features](#cool-features)
+  * [:clipboard: Status & To-Do List](#clipboard-status--to-do-list)
+  * [:clap: Inspiration](#clap-inspiration)
+  * [:envelope: Contact](#envelope-contact)
+
+## :books: General info
+
+* D3.js binds the [FCC json graph data](https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json) to the Document Object Model
+* Minified D3 is loaded directly from d3js.org using: `<script src="https://d3js.org/d3.v5.min.js"></script>`
+
+## :camera: Screenshots
+
+![Example screenshot](./img/plot.png).
+![Example screenshot](./img/postman.png).
+
+## :signal_strength: Technologies
+
+* [D3.js v5](https://d3js.org/) Data Driven Documents (D3) JavaScript library for manipulating documents based on data
+
+## :floppy_disk: Setup
+
+* Open  'index.html' in terminal to run program
+
+## :computer: Code Examples
+
+* extract from `script.js` to fetch json data from github API and extract a new date
+
+```javascript
+/*import json data using d3 v5 new method with then promise*/
+d3.json(
+	'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
+).then((data) => {
+	// extract time minutes and seconds as strings from json data then
+	// create a new date (1995, 1, 1, 1 are just placeholders)
+	data.forEach((d) => {
+		const parseMins = d.Time.slice(0, 2);
+		const parseSecs = d.Time.slice(3, 5);
+		d.Time = new Date(1995, 1, 1, 1, parseMins, parseSecs);
+	});
+```
+
+## :cool: Features
+
+* D3.js works with web standards so no plugins etc. required, just browser
+* D3.js has a rich toolset for data-driven visuals
+
+## :clipboard: Status & To-Do List
+
+* Status: Working
+* To-Do: nothing
+
+## :clap: Inspiration
+
+* [FreeCodeCamp Data Visualisation Project](https://www.freecodecamp.org/learn/data-visualization/data-visualization-projects/visualize-data-with-a-scatterplot-graph)
+
+## :envelope: Contact
+
+* Repo created by [ABateman](https://www.andrewbateman.org) - you are welcome to [send me a message](https://andrewbateman.org/contact)
---
+++ script.js
@@ -0,0 +1,167 @@
+/*set up container area*/
+const body = d3.select('body');
+const height = 400,
+	width = 700;
+const container = body
+	.append('svg')
+	.attr('height', height + 200 + 'px')
+	.attr('width', width + 100 + 'px');
+/*import json data using d3 v5 new method with then promise*/
+d3.json(
+	'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
+).then((data) => {
+	// extract time minutes and seconds as strings from json data then
+	// create a new date (1995 is just a placeholder)
+	data.forEach((d) => {
+		const parseMins = d.Time.slice(0, 2);
+		const parseSecs = d.Time.slice(3, 5);
+		d.Time = new Date(1995, 1, 1, 1, parseMins, parseSecs);
+	});
+
+	//function to open URL link in a new window
+	function openURL(d) {
+		const win = window.open(d.URL, '_blank');
+		win.focus();
+	}
+
+	/*vertical & horizontal scales*/
+	const verticalScale = d3
+		.scaleTime()
+		.domain(d3.extent(data, (d) => d.Time))
+		.range([0, height])
+		.nice(); //axis ends with round number
+	const minYear = d3.min(data, (d) => d.Year);
+	const horizontalScale = d3
+		.scaleLinear()
+		.domain([minYear - 1, d3.max(data, (d) => d.Year) + 1])
+		.range([0, width]);
+
+	/*y & x axes*/
+	const yAxis = d3
+		.axisLeft(verticalScale)
+		.tickFormat(d3.timeFormat('%M:%S'))
+		.ticks(d3.timeSecond, 10); //every 10 seconds
+	const xAxis = d3.axisBottom(horizontalScale).tickFormat(d3.format('d'));
+
+	/*plot area*/
+	const display = container.append('g').attr('transform', 'translate(70, 40)');
+
+	/*tooltip*/
+	const tooltip = body.append('div').attr('id', 'tooltip');
+
+	display
+		.selectAll('circle')
+		.data(data)
+		.enter()
+		.append('circle')
+		.attr('class', 'dot')
+		.attr('r', '4px')
+		.attr('data-xvalue', (d) => d.Year)
+		.attr('data-yvalue', (d) => d.Time)
+		.attr('cx', (d) => horizontalScale(d.Year))
+		.attr('cy', (d) => verticalScale(d.Time))
+		.attr('fill', (d) => (d.Doping == '' ? 'blue' : 'pink'))
+		.attr('stroke', 'grey')
+		.on('mouseover', (d) => {
+			const time = d.Time.toString().split(':');
+			const tooltipText =
+				d.Name +
+				', nationality: ' +
+				d.Nationality +
+				'<br/>' +
+				'Year: ' +
+				d.Year +
+				'<br/>' +
+				'Place: ' +
+				d.Place +
+				'<br/>' +
+				'Time: ' +
+				time[1] +
+				':' +
+				time[2].substring(0, 2) +
+				(d.Doping == ''
+					? '<br/>' + 'Doping: none'
+					: '<br/>' + 'Doping: ' + d.Doping);
+			tooltip.html(tooltipText);
+			tooltip
+				.attr('data-year', d.Year)
+				.style('left', `${d3.event.pageX + 5}px`)
+				.style('top', `${d3.event.pageY + 5}px`)
+				.transition()
+				.duration(500)
+				.style('opacity', 0.8);
+		})
+		.on('mouseout', () => {
+			tooltip.transition().duration(1500).style('opacity', 0);
+		})
+		.on('click', (d) => {
+			if (d.URL) openURL(d); //only applies to pink dots
+		});
+
+	/* y and x axes */
+
+	container
+		.append('g')
+		.attr('id', 'y-axis')
+		.attr('transform', 'translate(70, 40)')
+		.call(yAxis);
+	container
+		.append('g')
+		.attr('id', 'x-axis')
+		.attr('transform', 'translate(70, ' + (height + 40) + ')')
+		.call(xAxis);
+
+	/*legends to define coloured dots*/
+	const legend = display
+		.append('g')
+		.attr('id', 'legend')
+		.style('font-size', '18px')
+		.style('fill', 'blue')
+		.attr('height', 50)
+		.attr('width', 200)
+		.attr('transform', 'translate(' + (width - 300) + ', 0)');
+
+	legend
+		.append('text')
+		.text('Cyclists with no doping allegations')
+		.attr('transform', 'translate(0, 42)');
+
+	legend
+		.append('circle')
+		.attr('transform', 'translate(300, 0)')
+		.attr('r', 8)
+		.attr('class', 'legend')
+		.attr('fill', 'pink');
+
+	legend
+		.append('text')
+		.text('Cyclists with doping allegations')
+		.attr('transform', 'translate(0, 10)');
+
+	legend
+		.append('circle')
+		.attr('transform', 'translate(300, 35)')
+		.attr('r', 8)
+		.attr('class', 'legend')
+		.style('fill', 'blue');
+
+	//add x axis label
+	display
+		.append('g')
+		.append('text')
+		.attr('y', height + 20)
+		.attr('dy', '1em')
+		.style('font-size', '16px')
+		.attr('fill', 'blue')
+		.text('Year');
+	//add y axis label
+	display
+		.append('g')
+		.append('text')
+		.attr('x', -100)
+		.attr('dy', '-2.8em')
+		.attr('transform', 'rotate(-90)')
+		.style('font-size', '16px')
+		.attr('fill', 'blue')
+		.text('Time (seconds)');
+});
